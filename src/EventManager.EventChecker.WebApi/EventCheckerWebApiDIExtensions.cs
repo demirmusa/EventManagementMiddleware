46c4f3086a164a292456288d0947e5a889b6b523
musademir <demirmusa96@gmail.com>
2019-04-18 03:52:37 +0300

    error msg

--- src/EventManager.EventChecker.WebApi/EventCheckerWebApiDIExtensions.cs
+++ src/EventManager.EventChecker.WebApi/EventCheckerWebApiDIExtensions.cs
@@ -9,7 +9,7 @@ namespace EventManager.EventChecker.WebApi
     {
         public static IServiceCollection UseWebApiChecker(this IServiceCollection services, Action<WebApiOptions> webApiOptionsAction)
         {
-            throw new NotImplementedException();
+            throw new NotImplementedException("The option to check from web api has not yet been implemented.");
             services.AddTransient<IEventInfoLoader, WebApiEventChecker>();
             services.Configure(webApiOptionsAction);
             return services;
